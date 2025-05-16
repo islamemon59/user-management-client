889e0162bfc861906c1923ab015432827daf1fd8
islamemon59 <islamemon59@gmail.com>
2025-05-16 10:39:55 +0600

    first commit

---
+++ src/Component/AddUser.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const AddUser = () => {
+    return (
+        <div>
+            <Link></Link>
+            <div></div>
+        </div>
+    );
+};
+
+export default AddUser;
