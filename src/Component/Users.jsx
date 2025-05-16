878b82b00803bd628bed3544c7f0a1781871270b
islamemon59 <islamemon59@gmail.com>
2025-05-16 12:29:20 +0600

    user delete added

--- src/Component/Users.jsx
+++ src/Component/Users.jsx
@@ -2,6 +2,7 @@ import React, { use, useEffect } from "react";
 import Header from "./Header";
 import { Link, useLoaderData } from "react-router";
 import { AuthContext } from "../Context/Context";
+import Swal from "sweetalert2";
 
 const Users = () => {
   const initialUsers = useLoaderData();
@@ -9,6 +10,30 @@ const Users = () => {
   useEffect(() => {
     setUsers(initialUsers);
   }, [initialUsers, setUsers]);
+
+  const handleDeleteUser = (id) => {
+    fetch(`http://localhost:3000/users/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.deletedCount) {
+          Swal.fire({
+            position: "top-center",
+            icon: "success",
+            title: "Your work has been saved",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+          const remainingUser = users.filter(
+            (deleteUser) => deleteUser._id != id
+          );
+          setUsers(remainingUser);
+          console.log("response after user delete", data);
+        }
+      });
+  };
+
   return (
     <div>
       <Header></Header>
@@ -50,14 +75,25 @@ const Users = () => {
                       </div>
                     </div>
                   </td>
-                  <td>
-                    {user.email}
-                  </td>
+                  <td>{user.email}</td>
                   <td></td>
                   <td></td>
                   <th className="space-x-2">
-                    <button className="btn btn-xs font-bold text-xl"><img className="w-5 rounded-full" src="https://img.icons8.com/?size=64&id=56304&format=png" /></button>
-                    <button className="btn btn-xs font-bold text-xl pb-2 text-purple-500">x</button>
+                    <Link>
+                      {" "}
+                      <button className="btn btn-xs font-bold text-xl">
+                        <img
+                          className="w-5 rounded-full"
+                          src="https://img.icons8.com/?size=64&id=56304&format=png"
+                        />
+                      </button>
+                    </Link>
+                    <button
+                      onClick={() => handleDeleteUser(user._id)}
+                      className="btn btn-xs font-bold text-xl pb-2 text-purple-500"
+                    >
+                      x
+                    </button>
                   </th>
                 </tr>
               ))}
